f3c0e764da955c59977db5191c8fedf52612be49
Mannan Arora <m43arora@uwaterloo.ca>
2024-02-24 13:45:58 -0500

    Add search functionality

--- app/src/main/java/com/example/wat2eat/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/wat2eat/ui/home/HomeFragment.kt
@@ -1,20 +1,24 @@
 package com.example.wat2eat.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.wat2eat.api.RecipeResponse
+import com.example.wat2eat.api.RetrofitClient
 import com.example.wat2eat.databinding.FragmentHomeBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,12 +26,39 @@ class HomeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        binding.searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    searchRecipes(query)
+                } else {
+                    Toast.makeText(context, "Please enter a search query", Toast.LENGTH_SHORT).show()
+                }
+                return true
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //TODO
+                return true
+            }
+        })
+
         return root
+    }
+    private fun searchRecipes(query: String) {
+        val call = RetrofitClient.instance.searchRecipes(q = query)
+        call.enqueue(object: Callback<RecipeResponse> {
+            override fun onResponse(call: Call<RecipeResponse>, response: Response<RecipeResponse>) {
+                val responses = response.body()?.hits
+                Log.i("response:", responses.toString())
+            }
+            override fun onFailure(call: Call<RecipeResponse>, t: Throwable) {
+                print(t)
+            }
+        })
     }
 
     override fun onDestroyView() {
