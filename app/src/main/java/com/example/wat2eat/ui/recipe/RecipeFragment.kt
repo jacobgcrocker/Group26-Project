6aff5342b4b930b43a713045a4abd03a9d621299
Thanh Nguyen <ngthanh2248@gmail.com>
2024-02-27 07:05:13 -0500

    Finish UI

---
+++ app/src/main/java/com/example/wat2eat/ui/recipe/RecipeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.wat2eat.ui.recipe
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.wat2eat.R
+
+class RecipeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = RecipeFragment()
+    }
+
+    private lateinit var viewModel: RecipeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_recipe, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(RecipeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
