31de6309a48196d9d07ab3b10509a986a52036f4
Thanh Nguyen <ngthanh2248@gmail.com>
2024-03-31 15:54:18 -0400

    Styling

--- backend/src/controllers/recipe.ts
+++ backend/src/controllers/recipe.ts
@@ -15,27 +15,27 @@ export const createRecipe = async (
 };
 
 export const getRecipeById = async (
-  req: express.Request,
-  res: express.Response
+	req: express.Request,
+	res: express.Response
 ) => {
-  try {
-    const recipe = await Recipe.findById(req.params.recipeId);
-    res.status(200).json(recipe);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error });
-  }
-}
+	try {
+		const recipe = await Recipe.findById(req.params.recipeId);
+		res.status(200).json(recipe);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error });
+	}
+};
 
 export const getRecipesByIds = async (
-  req: express.Request,
-  res: express.Response
+	req: express.Request,
+	res: express.Response
 ) => {
-  try {
-    const recipes = await Recipe.find({ id: { $in: req.body.recipeIds } });
-    res.status(200).json(recipes);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error });
-  }
-}
+	try {
+		const recipes = await Recipe.find({ id: { $in: req.body.recipeIds } });
+		res.status(200).json(recipes);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error });
+	}
+};
