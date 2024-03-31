ad5a6cb55bd7ac12f976a33b4680d690ba4e47b9
Thanh Nguyen <ngthanh2248@gmail.com>
2024-03-31 17:20:49 -0400

    Register routes

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,11 +1,15 @@
-import express from "express";
-import health from "./health";
-import userData from "./userData";
+import express from 'express';
+import health from './health';
+import userData from './userData';
+import recipe from './recipe';
+import review from './review';
 
 const router = express.Router();
 
 export default (): express.Router => {
-  health(router);
-  userData(router);
-  return router;
+	health(router);
+	userData(router);
+	recipe(router);
+	review(router);
+	return router;
 };
