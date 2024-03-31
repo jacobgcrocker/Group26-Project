5780d323349f074067a8d2619e341ffa3675072d
Thanh Nguyen <ngthanh2248@gmail.com>
2024-03-31 15:33:35 -0400

    Add routes for getting recipe by id and getting recipes by ids

--- backend/src/controllers/recipe.ts
+++ backend/src/controllers/recipe.ts
@@ -13,3 +13,29 @@ export const createRecipe = async (
 		res.status(500).json({ error });
 	}
 };
+
+export const getRecipeById = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const recipe = await Recipe.findById(req.params.recipeId);
+    res.status(200).json(recipe);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+}
+
+export const getRecipesByIds = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const recipes = await Recipe.find({ id: { $in: req.body.recipeIds } });
+    res.status(200).json(recipes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+}
--- backend/src/router/recipe.ts
+++ backend/src/router/recipe.ts
@@ -1,6 +1,12 @@
 import express from 'express';
-import { createRecipe } from '../controllers/recipe';
+import {
+	createRecipe,
+	getRecipeById,
+	getRecipesByIds,
+} from '../controllers/recipe';
 
 export default (router: express.Router) => {
 	router.post('/recipe', createRecipe);
+	router.get('/recipe/:recipeId', getRecipeById);
+	router.get('/recipes', getRecipesByIds);
 };
