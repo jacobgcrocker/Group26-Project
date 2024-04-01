964e5b85f721ca823a364986153cf3be7322316e
Viktoria Never <vnever@uwaterloo.ca>
2024-03-31 22:36:23 -0400

    Connect to backend

---
+++ app/src/main/java/com/example/wat2eat/api/ReviewService.kt
@@ -0,0 +1,10 @@
+package com.example.wat2eat.api
+
+import com.example.wat2eat.models.Review
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+interface ReviewService {
+    @GET("/review")
+    fun getReviewsByRecipeId(@Query("recipeId") recipeId: String): Call<List<Review>>
+}
--- app/src/main/java/com/example/wat2eat/ui/reviews/ReviewViewModel.kt
+++ app/src/main/java/com/example/wat2eat/ui/reviews/ReviewViewModel.kt
@@ -4,6 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.wat2eat.models.Review
+import com.example.wat2eat.api.RetrofitClient
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class ReviewViewModel : ViewModel() {
     private val _reviewList = MutableLiveData<List<Review>>()
@@ -18,5 +23,20 @@ class ReviewViewModel : ViewModel() {
 
     fun updateReviewList(updatedReviews: List<Review>) {
         _reviewList.value = updatedReviews
+    }
+
+    fun fetchReviewsByRecipeId(recipeId: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val response: Response<List<Review>> = RetrofitClient.reviewServiceInstance.getReviewsByRecipeId(recipeId).execute()
+                if (response.isSuccessful) {
+                    _reviewList.postValue(response.body())
+                } else {
+                    println("Error: ${response.errorBody()?.string()}")
+                }
+            } catch (e: Exception) {
+                println("Exception: $e")
+            }
+        }
     }
 }
