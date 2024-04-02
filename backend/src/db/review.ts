f29ef600a040480163fcf127c0b6bb43d47cc994
Viktoria Never <vnever@uwaterloo.ca>
2024-04-02 19:32:31 -0400

    change review schema

--- backend/src/db/review.ts
+++ backend/src/db/review.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const ReviewSchema = new mongoose.Schema({
-  userId: { type: String },
-  username: {type: String},
-  recipeId: { type: Number },
-  reviewId: { type: Number },
-  content: { type: String },
-  rating: { type: Number },
+	userId: { type: String },
+	username: {type: String},
+	recipeId: { type: Number },
+	reviewId: { type: Number },
+	content: { type: String },
+	rating: { type: Number },
 });
 
 export const Review = mongoose.model("Review", ReviewSchema);
