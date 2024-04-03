84c4c6ca3abc5f17d8b64d35e80520ee17038df1
Tam Nguyen <tam.nguyen8232@gmail.com>
2024-04-03 02:31:32 -0400

    Dashboard to favourites

--- app/src/main/java/com/example/wat2eat/ui/favourites/FavouriteFragment.kt
+++ app/src/main/java/com/example/wat2eat/ui/favourites/FavouriteFragment.kt
@@ -1,4 +1,4 @@
-package com.example.wat2eat.ui.dashboard
+package com.example.wat2eat.ui.favourite
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.wat2eat.databinding.FragmentDashboardBinding
+import com.example.wat2eat.databinding.FragmentFavouriteBinding
 
-class DashboardFragment : Fragment() {
+class FavouriteFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentFavouriteBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val favouriteViewModel =
+                ViewModelProvider(this).get(FavouriteViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentFavouriteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textFavourite
+        favouriteViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/wat2eat/ui/favourites/FavouriteViewModel.kt
+++ app/src/main/java/com/example/wat2eat/ui/favourites/FavouriteViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.wat2eat.ui.dashboard
+package com.example.wat2eat.ui.favourite
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class FavouriteViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is Favourite Fragment"
     }
     val text: LiveData<String> = _text
 }
